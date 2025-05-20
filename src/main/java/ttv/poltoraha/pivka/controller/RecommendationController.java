222f9532ca8f9e5c883e60012e90019ce8f2b3eb
iwbar <sheksitv@mail.ru>
2025-05-20 22:58:32 +0300

    124

--- src/main/java/ttv/poltoraha/pivka/controller/RecommendationController.java
+++ src/main/java/ttv/poltoraha/pivka/controller/RecommendationController.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ttv.poltoraha.pivka.entity.Author;
 import ttv.poltoraha.pivka.entity.Book;
+import ttv.poltoraha.pivka.entity.Quote;
 import ttv.poltoraha.pivka.entity.Reader;
 import ttv.poltoraha.pivka.service.RecommendationService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/recommendation")
@@ -16,17 +20,17 @@ public class RecommendationController {
     private final RecommendationService recommendationService;
 
     @PostMapping("/recommendAuthor")
-    public void getRecommendAuthor(Reader reader){
-        recommendationService.recommendAuthor(reader.getUsername());
+    public List<Author> getRecommendAuthor(Reader reader){
+        return recommendationService.recommendAuthor(reader.getUsername());
     }
 
     @PostMapping("/recommendedBooks")
-    public void getRecommendBooks(Reader reader){
-        recommendationService.recommendBook(reader.getUsername());
+    public List<Book> getRecommendBooks(Reader reader){
+        return recommendationService.recommendBook(reader.getUsername());
     }
 
     @PostMapping("/recommendedBooks")
-    public void getRecommendQuoteByBook(Book book){
-        recommendationService.recommendQuoteByBook(book.getId());
+    public List<Quote> getRecommendQuoteByBook(Book book){
+        return recommendationService.recommendQuoteByBook(book.getId());
     }
 }
